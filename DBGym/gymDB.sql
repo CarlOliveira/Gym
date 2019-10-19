46ee2e882b8b1c83186f2063c76047a6519e0c1d
CarlOliveira <50527891+CarlOliveira@users.noreply.github.com>
2019-10-18 21:33:02 -0300

    Código SQL do Banco
    Código SQL do Banco

---
+++ DBGym/gymDB.sql
@@ -0,0 +1,37 @@
+create database gym;
+use gym;
+
+create table aluno(
+nome varchar(255),
+cpf varchar(14) not null primary key,
+telefone varchar(14),
+email varchar(255),
+endereco varchar(255)
+);
+
+create table instrutor(
+nome varchar(255),
+cpf_Instrutor varchar(14) not null primary key,
+telefone varchar(14),
+email varchar(255),
+endereco varchar(255),
+turno varchar(255)
+);
+
+create table exercicio(
+exercicio varchar(255) not null primary key,
+agrupamento_muscular varchar(255)
+);
+
+create table treino(
+cpf varchar(255),
+cpf_Instrutor varchar(255),
+exercicio varchar(255),
+descricao text,
+data_treino date,
+turno varchar(255),
+idTreino int not null auto_increment primary key,
+constraint alunoTreinoFK foreign key (cpf) references aluno(cpf),
+constraint InstrutorTreinoFK foreign key (cpf_Instrutor) references Instrutor(cpf_Instrutor),
+constraint ExercicioTreinoFK foreign key (exercicio) references Exercicio(exercicio)
+);
