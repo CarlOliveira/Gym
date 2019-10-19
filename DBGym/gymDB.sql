df8da0d99d28f948e8428cabf60416e8e8e86748
CarlOliveira <50527891+CarlOliveira@users.noreply.github.com>
2019-10-18 21:36:42 -0300

    Update gymDB.sql

--- DBGym/gymDB.sql
+++ DBGym/gymDB.sql
@@ -11,7 +11,7 @@ endereco varchar(255)
 
 create table instrutor(
 nome varchar(255),
-cpf_Instrutor varchar(14) not null primary key,
+cpf varchar(14) not null primary key,
 telefone varchar(14),
 email varchar(255),
 endereco varchar(255),
@@ -32,6 +32,6 @@ data_treino date,
 turno varchar(255),
 idTreino int not null auto_increment primary key,
 constraint alunoTreinoFK foreign key (cpf) references aluno(cpf),
-constraint InstrutorTreinoFK foreign key (cpf_Instrutor) references Instrutor(cpf_Instrutor),
+constraint InstrutorTreinoFK foreign key (cpf_Instrutor) references Instrutor(cpf),
 constraint ExercicioTreinoFK foreign key (exercicio) references Exercicio(exercicio)
 );
